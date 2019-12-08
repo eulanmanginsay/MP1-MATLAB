354c9fc4927bdf18373a8cf86466245aef903c4e
eulanmanginsay <57347025+eulanmanginsay@users.noreply.github.com>
2019-12-08 14:02:29 +0800

    Add files via upload

---
+++ MachineProblem1.m
@@ -0,0 +1,12 @@
+% Problem 1
+function x = MachineProblem1()
+f=(0:99);
+for n=1:100
+    if f(n)<=9;
+        f(n)=(f(n)^2)-7;
+    elseif f(n)>=10;
+        f(n)=f(n-10);
+    end
+end
+stem(f);
+end
